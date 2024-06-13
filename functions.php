ef0ffdb9ac529782c33698553f78a8115fab684b
David Peach <mail@davidpeach.co.uk>
2024-06-13 21:09:13 +0100

    Add multi query hook.

--- functions.php
+++ functions.php
@@ -15,4 +15,15 @@ function davidpeach_unhook_auto_excerpt()
     remove_filter('get_the_excerpt', 'wp_trim_excerpt');
 }
 
-require __DIR__.'/functions/post-types.php';
+add_filter('generateblocks_query_loop_args', function ($query_args, $attributes) {
+    if (
+        ! is_admin() &&
+        ! empty($attributes['className']) &&
+        strpos($attributes['className'], 'nine-multi-query') !== false
+    ) {
+        // pass meta_query parameter
+        $query_args['post_type'] = ['post', 'page'];
+    }
+
+    return $query_args;
+}, 10, 2);
